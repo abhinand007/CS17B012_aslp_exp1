11fbd4b747fe1dfee1fbcad107dc30b3724241a4
abhinand007 <33084097+abhinand007@users.noreply.github.com>
2019-01-18 17:53:48 +0530

    Add files via upload

---
+++ exp1_1.c
@@ -0,0 +1,10 @@
+
+int main() {
+	register int a = 5;
+	register int b = 7;
+	register int sum = a + b;
+	register int diff = b - a;
+	register int prod = a*b;
+	register int div = b/a;
+	return 0;
+}
